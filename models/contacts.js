350d10ad19e21fa3ff1e6f64d72dddaaa4948274
Mikhailo Salko <slipknotmisha@gmail.com>
2023-07-15 11:21:33 +0300

    add functions for working with contacts.json

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,54 @@
-// const fs = require('fs/promises')
+import fs from "fs/promises";
+import { nanoid } from "nanoid";
+import path from "path";
 
-const listContacts = async () => {}
+const contactPath = path.resolve("models", "contacts.json");
 
-const getContactById = async (contactId) => {}
+export const listContacts = async () => {
+  const buffer = await fs.readFile(contactPath);
+  return JSON.parse(buffer);
+};
 
-const removeContact = async (contactId) => {}
+export const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const foundContact = contacts.find((contact) => contact.id === contactId);
+  return foundContact || null;
+};
 
-const addContact = async (body) => {}
+export const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const contactIndex = contacts.findIndex((contact) => contact.id === contactId);
+  if (contactIndex === -1) {
+    return null;
+  }
+  const [deletedContact] = contacts.splice(contactIndex, 1);
+  await fs.writeFile(contactPath, JSON.stringify(contacts, null, 2));
+  return deletedContact;
+};
 
-const updateContact = async (contactId, body) => {}
+export const addContact = async ({ name, email, phone }) => {
+  const newContact = { id: nanoid(), name, email, phone };
+  const contacts = await listContacts();
+  contacts.push(newContact);
+  await fs.writeFile(contactPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
 
-module.exports = {
+export const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const contactIndex = contacts.findIndex((contact) => contact.id === contactId);
+  if (contactIndex === -1) {
+    return null;
+  }
+  contacts[contactIndex] = body;
+  await fs.writeFile(contactPath, JSON.stringify(contacts, null, 2));
+  return contacts[contactIndex];
+};
+
+export default {
   listContacts,
   getContactById,
   removeContact,
   addContact,
   updateContact,
-}
+};
