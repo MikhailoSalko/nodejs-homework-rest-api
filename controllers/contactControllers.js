89a7a442990f28db67bb085a3858c0302b388178
Mikhailo Salko <slipknotmisha@gmail.com>
2023-07-21 15:30:03 +0300

    update all controllers and add parch router and controller

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,5 +1,5 @@
 import { HttpErrorCreator, contactDecorator } from "../helpers/index.js";
-import { Contact } from "../schemas/index.js";
+import { Contact } from "../models/index.js";
 
 export const getAllContacts = async (req, res) => {
   const contacts = await Contact.find();
@@ -15,30 +15,43 @@ export const getContactById = async (req, res) => {
   res.json(searchContact);
 };
 
-// export const addNewContact = async (req, res) => {
-//   const newContact = await contactsService.addContact(req.body);
-//   res.status(201).json(newContact);
-// };
-// export const removeContactById = async (req, res) => {
-//   const deletedContact = await contactsService.removeContact(req.params.contactId);
-//   if (!deletedContact) {
-//     throw HttpErrorCreator(404, "Not Found");
-//   }
-//   res.json({ message: "contact deleted" });
-// };
+export const addNewContact = async (req, res) => {
+  const newContact = await Contact.create(req.body);
+  res.status(201).json(newContact);
+};
 
-// export const updateContactById = async (req, res) => {
-//   const updatedContact = await contactsService.updateContact(req.params.contactId, req.body);
-//   if (!updatedContact) {
-//     throw HttpErrorCreator(404, "Not Found");
-//   }
-//   res.json(updatedContact);
-// };
+export const removeContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const deletedContact = await Contact.findByIdAndRemove(contactId);
+  if (!deletedContact) {
+    throw HttpErrorCreator(404, "Not Found");
+  }
+  res.json({ message: "contact deleted" });
+};
+
+export const updateContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const updatedContact = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
+  if (!updatedContact) {
+    throw HttpErrorCreator(404, "Not Found");
+  }
+  res.json(updatedContact);
+};
+
+export const updateStatusContactById = async (req, res) => {
+  const { contactId } = req.params;
+  const updatedContact = await Contact.findByIdAndUpdate(contactId, req.body, { new: true });
+  if (!updatedContact) {
+    throw HttpErrorCreator(404, "Not Found");
+  }
+  res.json(updatedContact);
+};
 
 export default {
   getAllContacts: contactDecorator(getAllContacts),
   getContactById: contactDecorator(getContactById),
-  // addNewContact: contactDecorator(addNewContact),
-  // removeContactById: contactDecorator(removeContactById),
-  // updateContactById: contactDecorator(updateContactById),
+  addNewContact: contactDecorator(addNewContact),
+  removeContactById: contactDecorator(removeContactById),
+  updateContactById: contactDecorator(updateContactById),
+  updateStatusContactById: contactDecorator(updateStatusContactById),
 };
--- routes/api/contacts-routes.js
+++ routes/api/contacts-routes.js
@@ -9,14 +9,26 @@ router.get("/", contactControllers.getAllContacts);
 
 router.get("/:contactId", validateId, contactControllers.getContactById);
 
-// router.post("/", validateRequestBody(contactAddSchema), contactControllers.addNewContact);
+router.post(
+  "/",
+  validateRequestBody(contactJoiSchemas.contactAddSchema),
+  contactControllers.addNewContact
+);
 
-// router.delete("/:contactId", contactControllers.removeContactById);
+router.delete("/:contactId", validateId, contactControllers.removeContactById);
 
-// router.put(
-//   "/:contactId",
-//   validateRequestBody(contactAddSchema),
-//   contactControllers.updateContactById
-// );
+router.put(
+  "/:contactId",
+  validateId,
+  validateRequestBody(contactJoiSchemas.contactAddSchema),
+  contactControllers.updateContactById
+);
+
+router.patch(
+  "/:contactId",
+  validateId,
+  validateRequestBody(contactJoiSchemas.contactsFavoriteSchema),
+  contactControllers.updateStatusContactById
+);
 
 export default router;
