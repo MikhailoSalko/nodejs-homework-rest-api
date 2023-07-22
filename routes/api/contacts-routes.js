41fbd9606bd24193d7dac288e89048edf341dc19
Mikhailo Salko <114503196+MikhailoSalko@users.noreply.github.com>
2023-07-22 12:41:59 +0300

    Update contacts-routes.js - add /favorite to the end of the path in patch method

--- routes/api/contacts-routes.js
+++ routes/api/contacts-routes.js
@@ -25,7 +25,7 @@ router.put(
 );
 
 router.patch(
-  "/:contactId",
+  "/:contactId/favorite",
   validateId,
   validateRequestBody(contactJoiSchemas.contactsFavoriteSchema),
   contactControllers.updateStatusContactById
