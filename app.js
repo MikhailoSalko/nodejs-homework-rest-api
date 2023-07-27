f755edc98a9a80b4a8df79277644f75998dea238
Mikhailo Salko <slipknotmisha@gmail.com>
2023-07-27 16:18:00 +0300

    add user-router to app

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
-import dotenv from "dotenv";
+import "dotenv/config";
 
 import contactsRouter from "./routes/api/contacts-routes.js";
+import usersRouter from "./routes/api/users-routes.js";
 
 const app = express();
-dotenv.config();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -14,6 +14,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
