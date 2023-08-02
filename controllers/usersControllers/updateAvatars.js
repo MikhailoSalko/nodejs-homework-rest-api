9777f739f78623cdc42eb2f431915647c5437c44
Mikhailo Salko <slipknotmisha@gmail.com>
2023-08-02 16:11:11 +0300

    add jimp formatting for image

--- controllers/usersControllers/updateAvatars.js
+++ controllers/usersControllers/updateAvatars.js
@@ -1,18 +1,20 @@
 import { User } from "../../models/index.js";
 import path from "path";
 import fs from "fs/promises";
+import Jimp from "jimp";
 
 export const updateAvatars = async (req, res) => {
-  // console.log(req.file);
-  // console.log(req.user);
   const { _id } = req.user;
-  const { path: uploadPath, originalname } = req.file;
+  const { path: tempPath, originalname } = req.file;
   const fileName = `${_id}_${originalname}`;
-  // console.log(fileName);
   const avatarsPath = path.resolve("public", "avatars", fileName);
-  await fs.rename(uploadPath, avatarsPath);
   const avatarURL = path.join("avatars", fileName);
-  // console.log(avatarURL);
+
+  const image = await Jimp.read(tempPath);
+  await image.resize(250, 250);
+  await image.writeAsync(tempPath);
+
+  await fs.rename(tempPath, avatarsPath);
 
   await User.findByIdAndUpdate(_id, { avatarURL });
 
