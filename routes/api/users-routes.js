96d7cf00b4d14fa6809e4c5d6b37e0bb6e29fc83
Mikhailo Salko <slipknotmisha@gmail.com>
2023-08-09 17:19:10 +0300

    add get and post verify routes

--- routes/api/users-routes.js
+++ routes/api/users-routes.js
@@ -13,6 +13,14 @@ router.post(
   usersControllers.signup
 );
 
+router.get("/verify/:verificationToken", usersControllers.verifyEmail);
+
+router.post(
+  "/verify",
+  validateRequestBody(userJoiSchemas.userEmailSchema),
+  usersControllers.resendVerificationEmail
+);
+
 router.post(
   "/signin",
   validateRequestBody(userJoiSchemas.userSinginSchema),
@@ -24,7 +32,7 @@ router.get("/current", autorizationUser, usersControllers.getCurrent);
 router.post("/signout", autorizationUser, usersControllers.signout);
 
 router.patch(
-  "/",
+  "/subscription",
   autorizationUser,
   validateRequestBody(userJoiSchemas.userUpdateSubscription),
   usersControllers.updateUserSubscription
